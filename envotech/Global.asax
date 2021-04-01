5f3b39589f3d4c516b018ce849998ac69aebfa78
tarunverma23 <tarun.verma.23.tv@gmail.com>
2021-04-02 05:28:18 +1100

    initial

---
+++ envotech/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="envotech.MvcApplication" Language="C#" %>
---
+++ envotech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(envotech.Startup))]
+namespace envotech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
